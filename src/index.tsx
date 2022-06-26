df4fa316c5915a1a35c16ff1dce4fe19ba86cb15
Leonardo <leonardo.camarggo@outlook.com>
2022-06-26 20:41:21 -0300

    Create routes and config router

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,11 @@
-import App from "./App";
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import Routes from './router/paths';
+import ReactDOM from 'react-dom/client';
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Routes />
   </React.StrictMode>
 );
--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,5 +1,5 @@
 const App = () => {
-  return <div>Init</div>;
+  return <div>init</div>;
 };
 
 export default App;
