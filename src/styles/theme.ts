346a1a0168fdc4d5e7c87c9fc8f6212afad2d41b
Leonardo <leonardo.camarggo@outlook.com>
2022-07-31 20:20:05 -0300

    style(theme): edit name and hex of colors

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,7 +1,8 @@
 export default {
     colors: {
       black: '#000000',
-      darkGray: '#414141',
+      gray: '#414141',
+      darkGray: '#1A1A1A',
       green: '#02d463',
       white: '#ffffff'
     }
