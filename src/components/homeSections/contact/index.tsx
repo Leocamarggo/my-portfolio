25eb66463634aefbbd3d572b44a463254afb4f71
Leonardo <leonardo.camarggo@outlook.com>
2022-08-07 20:13:45 -0300

    feat(contact): create contact template

--- src/components/homeSections/contact/index.tsx
+++ src/components/homeSections/contact/index.tsx
@@ -1,7 +1,52 @@
 import * as T from './styles';
 
 const Contact = () => {
-  return <T.ContactContainer></T.ContactContainer>;
+  return (
+    <T.ContactContainer>
+      <T.ContactContent className="container">
+        <T.TitleContainer>
+          <T.TitleProject>contato</T.TitleProject>
+          <T.TitleLine />
+        </T.TitleContainer>
+
+        <T.BoxForm className="mt-5">
+          <T.FormContainer>
+            <T.RowNameAndEmail>
+              <div className="col-6">
+                <T.GenericInput
+                  required
+                  type="text"
+                  name="name"
+                  autoComplete="off"
+                  style={{ marginRight: 30 }}
+                />
+                <T.GenericLabel htmlFor="name">NOME</T.GenericLabel>
+              </div>
+              <div className="col-6">
+                <T.GenericInput
+                  required
+                  name="email"
+                  type="email"
+                  className="w-100"
+                  autoComplete="off"
+                />
+                <T.GenericLabel htmlFor="email">E-MAIL</T.GenericLabel>
+              </div>
+            </T.RowNameAndEmail>
+
+            <T.RowMessage>
+              <div className="col-12">
+                <T.MessageTextarea autoComplete="off" name="message" required />
+                <T.MessageLabel htmlFor="message">MENSAGEM</T.MessageLabel>
+              </div>
+            </T.RowMessage>
+
+            <T.SubmitButton>Enviar</T.SubmitButton>
+          </T.FormContainer>
+        </T.BoxForm>
+      </T.ContactContent>
+    </T.ContactContainer>
+  );
 };
 
 export default Contact;
