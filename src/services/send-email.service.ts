0aa9d1f9155bfd6ef3eafb94f9145d932894a6de
leo.camarggo <leonardo.silva@verot.com.br>
2023-08-02 11:10:34 -0300

    feat(api): update message for success or error in send email

--- src/services/send-email.service.ts
+++ src/services/send-email.service.ts
@@ -4,10 +4,10 @@ import { IEmailRequest } from 'interfaces/contact';
 export default class ContactService {
   static async sendEmail(data: IEmailRequest): Promise<void> {
     try {
-      const req = await api.post(`api/send-email`, {...data, email: `leonardo.camarggo@outlook.com`})
-      console.log(req)
+      await api.post(`api/send-email`, { ...data, email: `leonardo.camarggo@outlook.com` });
+      alert('Mensagem enviada com sucesso!')
     } catch (err) {
-      console.log(err);
+      alert('Oppss... Tivemos uma falha ao enviar a sua mensagem.\nPor favor, me contate de outra forma ou tente mais tarde!')
     }
   }
 }
